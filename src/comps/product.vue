16537a91cf85592978d139797ce0a19f9bc8e0e6
zhoujun <zhoujun@gomeplus.com>
2017-09-27 18:38:47 +0800

    t

--- src/comps/product.vue
+++ src/comps/product.vue
@@ -1,29 +1,52 @@
 <template>
   <div class="pro">
-     <div @click="go" class="pro1"><text>1</text></div>
-     <div class="pro1"><text>2</text></div>
-     <div class="pro1"><text>3</text></div>
+     <div @click="go(index)" v-for="(item,index) in datalist" :class="item.clist"><text>{{item.text}}</text></div>
   </div>
 </template>
-<style lang="less" scoped>
+<style scoped>
 .pro{
-  .pro1{
-    width:200px;
+   display:flex;
+   flex-direction: row
+}
+.pro1{
     height: 200px;
-    background-color:#656458;
-  }
+    background-color:#090;
+    flex-direction: row;
+    flex:1;
+}
+.active{
+    background-color:#423233;
 }
 </style>
 <script>
   export default {
     data () {
       return {
-      
+         shouldFixed:true,
+         clist:['pro1'],
+         datalist:[
+           {clist:['pro1'],text:'a'},
+           {clist:['pro1'],text:'b'},
+           {clist:['pro1'],text:'c'},
+           {clist:['pro1'],text:'d'}
+         ]
       }
     },
     methods:{
-      go(){
-        alert(90)
+      go(index){
+        var modal=weex.requireModule('modal')
+        modal.toast({
+          message:'i am a toast',
+          duration:1
+        })
+        this.datalist.forEach(item=>{
+         for(var i=0;i<item.clist.length;i++){
+           if(item.clist[i]=='active'){
+             item.clist=['pro1']
+           }
+         }
+        })
+        this.datalist[index].clist.push('active')
       }
     }
   }
