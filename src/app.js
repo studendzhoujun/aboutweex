067b783f418798bd7805cbbd1540c103e74ab96b
zhoujun <studentzhoujun@sina.com>
2017-12-26 15:57:34 +0800

    :tada: 目录结构

---
+++ src/app.js
@@ -0,0 +1,12 @@
+/*
+ * @Author: zhouJun 
+ * @Date: 2017-09-09 12:21:33 
+ * @Last Modified by: zhouJun
+ * @Last Modified time: 2017-12-26 15:55:30
+ */
+import app from './index.vue'
+// import api from './APIconfig.js'
+//  import store from './src/stores/index.js'
+app.el = '#root'
+console.log('===11')
+export default new Vue(app);
