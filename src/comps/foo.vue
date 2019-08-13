200bd65dd59fd914674a169c6a46f2f84802b18c
zhoujun <zhoujun147@jd.com>
2019-08-13 18:27:19 +0800

    :tada: img

--- src/comps/foo.vue
+++ src/comps/foo.vue
@@ -27,10 +27,10 @@
     data () {
       return {
         imageList: [
-          { src: '//gfs17.gomein.net.cn/T1zZAvBCAj1RCvBVdK_400.jpg'},
-          { src: '//gfs17.gomein.net.cn/T1hvJTBXYv1RCvBVdK_400.jpg'},
-          { src: '//gfs.gomein.net.cn/T17oKvBmYQ1RCvBVdK_400.jpg'},
-          {src:'//gfs17.gomein.net.cn/T19mYvBKZs1RCvBVdK_400.jpg'}
+          { src: '//m.360buyimg.com/mobilecms/s750x750_jfs/t1/46760/17/6465/378314/5d410eb8Ee0438775/866310e391656ef5.jpg'},
+          { src: '//m.360buyimg.com/mobilecms/s720x720_jfs/t1/50710/39/6334/332198/5d410ea3E6bb31f90/0682796778ab70f3.jpg'},
+          { src: '//m.360buyimg.com/mobilecms/s720x720_jfs/t1/83098/25/5908/385913/5d410e6eE88df52c4/b1bff3b032b7ed9b.jpg'},
+          {src:'//m.360buyimg.com/mobilecms/s720x720_jfs/t1/83981/31/5907/382990/5d410ea2E299558ca/ecc91cd381949228.jpg'}
         ]
       }
     },
